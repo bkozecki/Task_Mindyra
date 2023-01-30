0f07f395cc78ba28a16d447394b8c277941bef89
Bartosz Kozecki <bartoszkozecki@192.168.1.5>
2023-01-30 15:18:55 +0100

    final commit

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -15,22 +15,24 @@ const Slider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    if (currentIndex > 4) {
-      setCurrentIndex(0);
-    }
-    if (currentIndex < 0) {
-      setCurrentIndex(4);
-    }
     fetch(`https://api.github.com/users/${usernames[currentIndex]}`)
       .then((res) => res.json())
       .then((data) => setCurrentImg(data.avatar_url));
   }, [currentImg, currentIndex]);
 
   const nextImg = () => {
+    if (currentIndex > 4) {
+      setCurrentIndex(0);
+      return;
+    }
     setCurrentIndex((prevState) => prevState + 1);
   };
 
   const prevImg = () => {
+    if (currentIndex < 0) {
+      setCurrentIndex(4);
+      return;
+    }
     setCurrentIndex((prevState) => prevState - 1);
   };
 
